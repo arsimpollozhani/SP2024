589c7849da1ee69dccb5d335f894edb7dc165cf6
Arsim Pollozhani <pollozhaniarsim@gmail.com>
2025-02-08 20:59:15 +0100

    add codeforces problem

---
+++ codeforce.cpp
@@ -0,0 +1,29 @@
+//
+// Created by win11pro on 2/8/2025.
+//
+
+#include<iostream>
+#include<cstring>
+
+void normalize (char * str) {
+    for(int i = 0; i < strlen(str); i++) {
+        str[i] = tolower(str[i]);
+    }
+}
+
+using namespace std;
+int main () {
+    char str[4];
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> str;
+        normalize(str);
+        if(strcmp(str, "yes") == 0) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -2,25 +2,8 @@
 using namespace std;
 
 int main() {
-    int matrix[100][100];
-    int m;
-
-    cin >> m;
-
-    // Read the matrix elements
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> matrix[i][j];
-        }
-    }
-
-    // Print the matrix
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+    int a;
+    cin >> a;
 
     return 0;
 }
